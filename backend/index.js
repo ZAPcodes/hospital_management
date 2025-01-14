7a94b50b31e27d443b6449df4e3c1e5471ccd51e
ZAPcodes <techboyabhinav@gmail.com>
2025-01-15 01:11:23 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ async function startServer() {
         });
 
         // Start server
-        const PORT = process.env.PORT || 8000;
+        const PORT = 10000;
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         }).on('error', (err) => {
